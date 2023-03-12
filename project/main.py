0852d63f6d52d3bffc85f3699268365e05e1a089
Graziele-Rodrigues <c.graziele.rodrigues@gmail.com>
2023-03-12 17:51:24 -0300

    ajustes

--- project/main.py
+++ project/main.py
@@ -1,15 +1,15 @@
 from functions import Functions
 import pandas as pd
 
-arquivo = Functions("Anexo/critical_path/TOY.csv")
-#arquivo = Functions("C:/Users/grazi/OneDrive/Área de Trabalho/Arquivos/CaminhoCriticoEmProjetos/project/Anexo/critical_path/TOY.csv")
+#arquivo = Functions("Anexo/critical_path/TOY.csv")
+arquivo = Functions("C:/Users/grazi/OneDrive/Área de Trabalho/Arquivos/CaminhoCriticoEmProjetos/project/Anexo/critical_path/CienciasBiologicas.csv")
 # print(arquivo.print_table())
 
 print(arquivo.print_table())
 print(arquivo.graph.adj_list)
 print(arquivo.graph.dijkstra_max(0))
 dijkstra_max = arquivo.graph.dijkstra_max(0)
-path_critical = arquivo.graph.get_critical_path(dijkstra_max[1], 8)
+path_critical = arquivo.graph.get_critical_path(dijkstra_max[1], (len(dijkstra_max[1])-2))
 print(path_critical)
 
 # mostrando disciplinas criticas
